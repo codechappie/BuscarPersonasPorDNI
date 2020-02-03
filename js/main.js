0bcbb321670330226a12ef7e9e21e3a843163b1f
codechappie <davidproyecto01@gmail.com>
2020-02-02 20:45:33 -0500

    Upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InZlaGVuYWsxMzFAbWFpbHJ1bm5lci5uZXQifQ.0jrPOYk0vLIA3Z2UXbkKdtGC3RXHUrdpKxD7D-cGDFQ";
+const simpleForm = document.getElementById("simpleForm");
+let fatherSurname = document.getElementById("fatherSureName");
+let motherSurname = document.getElementById("motherSurName");
+let name = document.getElementById("name");
+let dniNumber = document.querySelectorAll(".dniNumber");
+let newDni;
+
+simpleForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    newDni = document.getElementById("newDni").value;
+    consultingNewPersonPerDNI(newDni);
+    console.log(newDni)
+});
+
+window.onload = ()=>{
+    noneData();
+};
+    
+
+function consultingNewPersonPerDNI(newDni){
+    console.log(newDni)
+    const url = `http://dniruc.apisperu.com/api/v1/dni/${newDni}?token=${key}`; 
+    fetch(url)
+    .then((response)=>{
+        if(response.ok){
+            return response.json();
+        }else{
+            console.log("error")
+        }
+    }).then((data)=>{
+        dniNumber.forEach( dni => {
+            dni.innerHTML=data.dni;
+        });
+        fatherSurname.innerHTML=data.apellidoPaterno;
+        motherSurname.innerHTML=data.apellidoMaterno;
+        name.innerHTML=data.nombres;
+    }).catch((err)=>{
+        console.log(err);
+        noneData();
+    });
+}
+
+function noneData(){
+    dniNumber.forEach( dni => {
+        dni.innerHTML="---------";
+    });
+    fatherSurname.innerHTML="----------";
+    motherSurname.innerHTML="----------";
+    name.innerHTML="-------";
+}
